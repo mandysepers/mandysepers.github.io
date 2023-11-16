96ac37c6090998a02fb47cd4c0b1972345c75f90
GROUPINFRA\sepersm <mandy.sepers@cgi.com>
2023-11-16 20:51:38 +0100

    quiz data opzet

---
+++ quizData.js
@@ -0,0 +1,97 @@
+// Quiz Data
+var allQuizData = {
+    "ExampleSet": [
+        {
+            question: "Wat is het geboortejaar van X?",
+            options: ["1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000"],
+            correctAnswer: ""
+        },
+        {
+            question: "Wat is de geboortemaand van X?",
+            options: ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+            correctAnswer: ""
+        },
+        {
+            question: "Wat de geboortedag van X?",
+            options: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
+            correctAnswer: ""
+        },
+        {
+            question: "Hoe oud is X?",
+            optons: ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"],
+            correctAnswer: ""
+        },
+
+    ],
+    "Alex": [
+        {
+            question: "Wat is het geboortejaar van X?",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+    ],
+    "Johan": [
+        {
+            question: "Sample Question 1? Johan",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+    ],
+    "Maureen": [
+        {
+            question: "Sample Question 1? maureen",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+    ],
+    "Myrna": [
+        {
+            question: "Sample Question 1? Myrna",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+    ],
+    "Patrick": [
+        {
+            question: "Sample Question 1? Patrick",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+    ],
+    "Ria": [
+        {
+            question: "Sample Question 1? Ria",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+        {
+            question: "Sample Question 2? Ria",
+            options: ["Option A", "Option B", "Option C"],
+            correctAnswer: "Option B"
+        },
+    ],
+    "Romy": [
+        {
+            question: "Sample Question 1? Romy",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+        {
+            question: "Sample Question 2? Romy",
+            options: ["Option A", "Option B", "Option C"],
+            correctAnswer: "Option B"
+        },
+    ],
+    "Timo": [
+        {
+            question: "Sample Question 1? timo",
+            options: ["Option 1", "Option 2", "Option 3"],
+            correctAnswer: "Option 1"
+        },
+        {
+            question: "Sample Question 2? timo",
+            options: ["Option A", "Option B", "Option C"],
+            correctAnswer: "Option B"
+        },
+    ]
+};
