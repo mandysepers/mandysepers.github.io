be05706d0e8ecbec49e05a83993be5a56a01fd7d
GROUPINFRA\sepersm <mandy.sepers@cgi.com>
2023-12-22 12:36:26 +0100

    quizdata for johan

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ var allQuizData = {
             options: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
             correctAnswer: ""
         },
-        {
-            question: "Hoe oud is X?",
-            options: ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"],
-            correctAnswer: ""
-        },
+        // {
+        //     question: "Hoe oud is X?",
+        //     options: ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"],
+        //     correctAnswer: ""
+        // },
         {
             question: "Hoe lang is X?",
             options: ["160","161","162","163","164","165","166","167","168","169","170","171","172","173","174","175","176","177","178","179","180","181","182","183","184","185","186","187","188","189","190","191","192","193","194","195","196","197","198","199","200"],
@@ -106,13 +106,13 @@ var allQuizData = {
             options: ["0","1","2","3","4","5","6","7","8","9","10"],
             correctAnswer: ""
         },
-        {
-            question: "van X?",
-            options: [
+        // {
+        //     question: "van X?",
+        //     options: [
                 
-                ],
-            correctAnswer: ""
-        }
+        //         ],
+        //     correctAnswer: ""
+        // }
     ],
     "Alex": [
         {
@@ -220,10 +220,102 @@ var allQuizData = {
     ],
     "Johan": [
         {
-            question: "Sample Question 1? Johan",
-            options: ["Option 1", "Option 2", "Option 3"],
-            correctAnswer: "Option 1"
+            question: "Wat is het geboortejaar van Maureen?",
+            options: ["1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000"],
+            correctAnswer: "1994"
         },
+        {
+            question: "Wat is de geboortemaand van Romy?",
+            options: ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+            correctAnswer: "juli"
+        },
+        {
+            question: "Wat de geboortedag van Alex?",
+            options: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
+            correctAnswer: "22"
+        },
+        {
+            question: "Hoe lang is Patrick?",
+            options: ["160","161","162","163","164","165","166","167","168","169","170","171","172","173","174","175","176","177","178","179","180","181","182","183","184","185","186","187","188","189","190","191","192","193","194","195","196","197","198","199","200"],
+            correctAnswer: "174"
+        },
+        {
+            question: "Wat is de schoenmaat van Timo?",
+            options: ["35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50"],
+            correctAnswer: "46"
+        },
+        {
+            question: "Wat is de lievelingskleur van Ria?",
+            options: ["1. Roze, 2. paars en 3. oranje","Blauw","Donkerblauw","Flessengroen","Geel","Groen","Roest","Rood","Zwart"],
+            correctAnswer: "Zwart"
+        },
+        {
+            question: "Waarheen ging de laatste vakantie van Myrna?",
+            options: ["Egypte", "Frankrijk", "Maleisië", "Nederland", "Spanje"],
+            correctAnswer: "Maleisië"
+        },
+        {
+            question: "Wat is het favoriete seizoen van Maureen?",
+            options: ["Winter","Lente","Zomer","Herfst", "Alle seizoenen zijn prima"],
+            correctAnswer: "Lente"
+        },        {
+            question: "Wat is de favoriete TV-serie allertijden van Romy?",
+            options: ["Breaking bad","Engelse detective series","Goede tijden, slechte tijden","Handmaids tale/married at first sight australie","Mc Gyver","Meiland/de augurkenkoning","VI","Wie is de mol?"],
+            correctAnswer: "Handmaids tale/married at first sight australie"
+        },
+        {
+            question: "Wat is het lievelingseten van Alex?",
+            options: ["Brood","Chinees","Pasta","Pasta bolognese","Sate met opgepakte aardappelen","Stoofperen","Sushi en frikandellen"],
+            correctAnswer: "Pasta"
+        },
+        {
+            question: "Wat is het favoriete nummer en/of artiest van Patrick?",
+            options: ["Heb ik niet echt, zou zeggen andre hazes","I want it that way, backstreet boys","Laatste rondje - Hazes. (het is tijd, de hoogste tijd...)","Paradise by the dashboardlight - meat loaf","Vanilla ice (ice baby)","Queen, I want to break free"],
+            correctAnswer: "Laatste rondje - Hazes. (het is tijd, de hoogste tijd...)"
+        },
+        {
+            question: "Als Timo zou moeten emigreren, waar zou die dan heen gaan?",
+            options: ["Bali (Indonesie)","Indonesië","Italie","Italië of Spanje, naar m'n zus","Nergens","Spanje","Spanje, Mallorca"],
+            correctAnswer: "Spanje"
+        },
+        {
+            question: "Wat is de lievelingssport van Alex?",
+            options: ["Geen!","Golf","Schaatsen","Shoppen","Softbal","Voetbal"],
+            correctAnswer: "Voetbal"
+        },
+        {
+            question: "Wat is het verborgen talent van Myrna?",
+            options: ["Dat ik heel goed dingen kan regelen (zoals een hele vakantie!)","Eigenlijk ben ik heel slim","Enorm gevoelig","Heb ik niet","Heb ik niet, ze liggen al op tafel","Ik kan met mn tong mn neus aanraken","Mijzelf zijn","Té goed verborgen"],
+            correctAnswer: "Dat ik heel goed dingen kan regelen (zoals een hele vakantie!)"
+        },
+        {
+            question: "Wie is het idool van Ria?",
+            options: ["Al mijn familie","Geen","Geraldine kemper","Harrison Ford","De vader van Mandy","Me vader","Sam smith","Vader en zoon Verhulst"],
+            correctAnswer: "Al mijn familie"
+        },
+        {
+            question: "Hoe veel broers/zussen heeft/had Patrick?",
+            options: ["0","1","2","3","4","5","6"],
+            correctAnswer: "2"
+        },
+        {
+            question: "Wat voor auto heeft Maureen?",
+            options: ["Kia Niro","Mazda mx30","Renault clio","Renault Megane E Tech","Toyota Aygo","Volvo"],
+            correctAnswer: "Toyota Aygo"
+        },
+
+        {
+            question: "Hoe veel keer is Romy verhuisd?",
+            options: ["0","1","2","3","4","5","6","7","8","9","10"],
+            correctAnswer: "5"
+        },
+        // {
+        //     question: "van X?",
+        //     options: [
+                
+        //         ],
+        //     correctAnswer: ""
+        // }
     ],
     "Maureen": [
         {
